d727cfb3356c43a4076a1581d5c505ba515bb089
Genta IHA <genta@teeda.jp>
2009-08-23 15:21:36 +0000

    updated

--- bgpwatch.rb
+++ bgpwatch.rb
@@ -3,10 +3,36 @@
 
 require 'pp'
 
-class TwitterClient
+# Notify BGP status to another network service
+class Notifier
 end
 
-class BGPWatch
+# IRC Client
+class IRCClient
 end
 
-pp BGPWatch.new
+# connect to Quagga/Zebra vty, and fetch peer status
+class PeerWatcher
+  def initialize
+  end
+
+  def get
+  end
+end
+
+# BGPWatcher main class. Source of meta-programming.
+class BGPWatchStub
+  def initialize
+  end
+
+  def run
+  end
+end
+
+
+
+# Real instance to work.
+class BGPWatch < BGPWatchStub
+end
+
+pp BGPWatch.new.run

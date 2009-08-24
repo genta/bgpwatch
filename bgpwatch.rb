44bb03f428b9908347e866f0608fcff0fab84c73
Genta IHA <genta@teeda.jp>
2009-08-24 20:26:47 +0000

    updated

--- bgpwatch.rb
+++ bgpwatch.rb
@@ -24,39 +24,61 @@ end
 # BGPWatcher main class. The source of the meta-programming.
 class BGPWatchStub
   def initialize
+    @notifier = self.class.notifier
+    @watcher = self.class.watcher
   end
 
   def run
+    pp @notifier
+    pp @watcher
+    'done'
   end
 
+=begin
   # クラスのインスタンス変数へのアクセサメソッド
   # メタ化できそうだ
-  def self.notifier(param)
+  def self.notifier(*param)
     return @notifier if param.empty?
     
     # クラスのインスタンス変数に，paramを保存しておく．
     # そこしか使えないからな．
     @notifier ||= {}
-    @notifier = {:class => IRCClient}.merge(param)
+    @notifier = {:class => Notifier::IRCClient}.merge(*param)
   end
 
   # クラスのインスタンス変数へのアクセサメソッド
   # メタ化できそうだ
-  def self.watcher(param)
+  def self.watcher(*param)
     return @watcher if param.empty?
 
     # クラスのインスタンス変数に，paramを保存しておく．
     # そこしか使えないからな．
     @watcher ||= {}
-    @watcher = {}.merge(param)
+    @watcher = {}.merge(*param)
   end
+=end
+
+  def self.attributes(*params)
+    return @attributes if params.empty?
+
+    params[0].each do |attr_name, default|
+      attr_accessor attr_name
+      meta_def(attr_name) do
+        # To be done
+        # self.instance_variable_set(attr_name, default)
+      end
+    end
+  end
+
+  attributes :notifier => {:class => Notifier::IRCClient},
+             :watcher => {}
 end
 
 
 
 # Real instance to work.
 class BGPWatch < BGPWatchStub
-  notifier :class => IRCClient, 
+  notifier :class => Notifier::IRCClient, 
            :server => 'irc.reicha.net:6667', 
            :nick => 'ihanetbot'
   watcher  :server => 'localhost'

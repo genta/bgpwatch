71dcc588350e7f041129129d93e0d004f1dbec85
Genta IHA <genta@teeda.jp>
2009-08-25 17:12:16 +0000

    updated

--- bgpwatch.rb
+++ bgpwatch.rb
@@ -10,15 +10,27 @@ module Attributes
   def attributes(*params)
     return @attributes if params.empty?
 
-    params.each do |attr|
-      attr_accessor attr
-      meta_def(attr) do |value|
+    params.each do |attr_name|
+      attr_accessor attr_name
+      meta_def(attr_name) do |value|
+        traversally_new = lambda {|item|
+          if item.is_a?(Array) then
+            return item.map {|val| traversally_new.call(val)}
+          else
+            return item.is_a?(Class) ? item.new : item
+          end
+        }
+        value = traversally_new.call(value)
+
         @attributes ||= {}
-        @attributes[attr] = value
+        @attributes[attr_name] = value
       end
     end
 
     class_def(:initialize) do
+      init_attributes
+    end
+    class_def(:init_attributes) do
       self.class.attributes.each do |key, value|
         instance_variable_set("@#{key}", value)
       end
@@ -82,13 +94,13 @@ end
 
 class MyWatcher < Watcher::Quagga
   server 'localhost'
-  storage MyStorage.new
+  storage MyStorage
 end
 
 
 class MyBGPWatch < BGPWatch
-  notifier MyNotifier.new
-  watcher  MyWatcher.new
+  notifier MyNotifier
+  watcher  MyWatcher
 end
 
 pp MyBGPWatch.new.run

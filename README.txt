a82a20b1525b3bd5ba7c12f0b0abf5accbcd20c2
Genta IHA <genta@teeda.jp>
2009-08-27 12:36:57 +0000

    updated

---
+++ README.txt
@@ -0,0 +1,7 @@
+= requirements
+
+This software depends these sofwares:
+
+  - rubygems
+  - gem packages as follows:
+    - metaid
--- bgpwatch.rb
+++ bgpwatch.rb
@@ -1,11 +1,17 @@
 #!/usr/local/bin/ruby
 # $Id$
 
+require 'rubygems'
 require 'pp'
 require 'metaid'
+require 'ruby-debug'
+
+DEBUG = true
 
 
-# source of the meta-programming.
+#
+# Attribute module. The source of the meta-programming.
+#
 module Attributes
   def attributes(*params)
     return @attributes if params.empty?
@@ -38,69 +44,243 @@ module Attributes
   end
 end
 
+module Runnable
+  def run
+  end
 
+  def shutdown
+  end
+end
+
+
+#
 # Notifier bridge module
+#
 module Notifier
+  class BasicNotifier
+    include Runnable
+
+    # notify message via the notifier object.
+    def notify(msg)
+      puts "BasicNotifier says: #{msg}"
+    end
+  end # Notifier::BasicNotifier
+
+  #
   # IRC Client. Client class of Notifier
-  class IRCClient
+  #
+  class IRCClient < BasicNotifier
     extend Attributes
     attributes :server, :port, :nick
   end # Notifier::IRCClient
 end # Notifier
 
 
+=begin
+module PeerStatus
+  Up = Object.new
+  Down = Object.new
+  attr :asnum_table
+
+  def initialize
+    @asnum_table = Hash.new
+  end
+
+  # returns: Array of PeerStatus::Entry
+  def ipaddr(ipaddr)
+    @storage[ipaddr]
+  end
+
+  # returns: Array of PeerStatus::Entry
+  def asnum(asnum)
+    @asnum_table[asnum]
+  end
+
+  class Entry < Hash
+    @storage[ipaddr] = {:asnum => 64512, :status => PeerStatus::Up}
+
+    # key_attr :ipaddr
+    # attr :asnum
+    # attr :since_last_event
+    # attr :status # PeerStatus::Up / PeerStatus::Down
+  end
+end
+
+class Event
+  attr :event  # PeerStatus::Up / PeerStatus::Down
+  attr :conn   # PeerStatus::Entry
+end
+=end
+
+Struct.new('PeerStatus', :ipaddr, :asnum, :since_last_event, :status)
+
+
+#
 # Peer status watcher module
+# 
 module Watcher
+  #
   # connect to Quagga/Zebra vty, and fetch peer status
+  #
   class Quagga
     extend Attributes
-    attributes :server, :storage
+    attributes :server, :user, :password
+    include Runnable
+
+    def execute(cmd)
+      return "(my result\n)"
+    end
+
+    def get_peers
+      x = Struct.PeerStatus.new('fe80::nork:1', 64530, '00:00:01', 'Down')
+      y = Struct.PeerStatus.new('fe80::ume:1', 64520, '00:00:15', 'Up')
+      return [x, y]
+    end
   end # Watcher::Quagga
+
+  #
+  # Manager class of Watcher.
+  #
+  class Manager
+    extend Attributes
+    attributes :watcher, :storage
+    include Runnable
+
+    # get 'show ipv6 bgp' result.
+    # returns: Array
+    def get_peers
+      @watcher.get_peers
+    end
+
+    # check events.
+    # retruns:
+    #   Array of message (If some errors)
+    #   nil (If there is no trouble)
+    def check
+      result = get_peers
+    end
+  end
 end # Watcher
 
 
+#
 # store peer status permernently
-class Storage
-  extend Attributes
-  attributes :file
-end
+#
+module Storage
+  class BasicStorage < Hash
+    include Runnable
 
+    # open: do nothing
+    def open
+    end
 
-# BGPWatcher main class.
-class BGPWatch
-  extend Attributes
-
-  def run
-    pp @notifier
-    pp @watcher
-    'done'
+    # close: do nothing
+    def close
+    end
   end
 
+  class FileStorage < BasicStorage
+    extend Attributes
+    attributes :file
+
+    # open from file and restore datas.
+    def open
+      puts "FileStorage: opened."
+    end
+
+    # store data into file.
+    def flush
+      puts "FileStorage: flushed"
+    end
+
+    # store data into file, and close.
+    def close
+      flush
+      puts "FileStorage: closed."
+    end
+
+    def shutdown
+      close
+    end
+  end
+end
+
+
+#
+# BGPWatcher main class.
+#
+class BGPWatch
+  extend Attributes
   attributes :notifier, :watcher
+  include Runnable
+
+  # checker method.
+  # returns:
+  #   false: There are no warnings
+  #   Array: There are errors
+  def check
+    return @watcher.check
+  end
+
+  # output message via notifier.
+  # receives: Array of messages.
+  # returns: nil
+  def notify(msg)
+    msg.each {|message| @notifier.notify(message)}
+  end
+
+  # start watching.
+  # returns: never return.
+  def run
+    daemonize
+    [@notifier, @watcher].each {|process| process.run}
+
+    loop do
+      if (result = check) then
+        notify(result)
+      end
+      sleep 60
+    end
+  end
+
+  private
+  # daemonize myself.
+  def daemonize
+    puts "(daemonize!)" if DEBUG
+  end
 end
 
 
 
-# Real instance to work.
+#
+# Real instances to work.
+# 
+
 class MyNotifier < Notifier::IRCClient
   server 'irc.reicha.net'
   port 6667
   nick 'ihanetbot'
 end
 
-class MyStorage < Storage
+class MyStorage < Storage::FileStorage
   file '/tmp/mystorage.db'
 end
 
 class MyWatcher < Watcher::Quagga
   server 'localhost'
+  user 'login'
+  password 'password'
+end
+
+class MyWatchManager < Watcher::Manager
+  watcher MyWatcher
   storage MyStorage
 end
 
-
 class MyBGPWatch < BGPWatch
   notifier MyNotifier
-  watcher  MyWatcher
+  watcher MyWatchManager
 end
 
-pp MyBGPWatch.new.run
+bgpwatcher = MyBGPWatch.new
+pp bgpwatcher.run
